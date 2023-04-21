fadc723781578b7dd80f5327842fa44e99433bfd
Crystal-Rosenbrook <129122210+Crystal-Rosenbrook@users.noreply.github.com>
2023-04-20 19:29:26 -0500

    Main script for analysis

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,79 +2,60 @@ import os
 import csv
 
 # Define the path for the input file
-csvpath = r"C:\Users\cryst\Documents\UMN Data Visualization and Analytics Bootcamp\Coursework\Section 2 Data Analytics with Python\Module 3\Challenge Files\Starter_Code\PyPoll\Resources\election_data.csv"
+csvpath = r"C:\Users\cryst\Documents\UMN Data Visualization and Analytics Bootcamp\Coursework\Section 2 Data Analytics with Python\Module 3\Challenge Files\Starter_Code\PyBank\Resources\budget_data.csv"
 
 # Initialize variables
-total_votes = 0
-candidates = {}
-winner_votes = 0
-winner = ""
+months = []
+profit_losses = []
+changes = []
 
 # Open the input file
 with open(csvpath, newline='') as csvfile:
-    
+
     # Read the input file
     csvreader = csv.reader(csvfile, delimiter=',')
-    
+
     # Skip the header row
     header = next(csvreader)
-    
-    # Loop through the data
+
+    # Loop through the data and store in the lists
     for row in csvreader:
-        
-        # Count the total number of votes cast
-        total_votes += 1
-        
-        # Count the number of votes for each candidate
-        candidate_name = row[2]
-        if candidate_name in candidates:
-            candidates[candidate_name] += 1
-        else:
-            candidates[candidate_name] = 1
-            
-# Print the analysis to the terminal
-print("Election Results")
-print("-------------------------")
-print(f"Total Votes: {total_votes}")
-print("-------------------------")
+        months.append(row[0])
+        profit_losses.append(int(row[1]))
 
-# Initialize the output string
-output_string = "Election Results\n"
-output_string += "-------------------------\n"
-output_string += f"Total Votes: {total_votes}\n"
-output_string += "-------------------------\n"
+# Calculate the total number of months and the net total amount of "Profit/Losses"
+total_months = len(months)
+net_total = sum(profit_losses)
 
-# Loop through the candidates
-for candidate_name in candidates:
-    
-    # Calculate the percentage of votes
-    candidate_votes = candidates[candidate_name]
-    candidate_percentage = candidate_votes / total_votes * 100
-    
-    # Print the candidate results to the terminal
-    print(f"{candidate_name}: {candidate_percentage:.3f}% ({candidate_votes})")
-    
-    # Update the winner if needed
-    if candidate_votes > winner_votes:
-        winner_votes = candidate_votes
-        winner = candidate_name
-        
-    # Append the candidate results to the output string
-    output_string += f"{candidate_name}: {candidate_percentage:.3f}% ({candidate_votes})\n"
-    
-# Print the winner to the terminal
-print("-------------------------")
-print(f"Winner: {winner}")
-print("-------------------------")
+# Calculate the changes in "Profit/Losses" over the entire period, and then the average of those changes
+for i in range(1, len(profit_losses)):
+    changes.append(profit_losses[i] - profit_losses[i-1])
+average_change = sum(changes) / len(changes)
 
-# Append the winner to the output string
-output_string += "-------------------------\n"
-output_string += f"Winner: {winner}\n"
-output_string += "-------------------------\n"
+# Find the greatest increase and decrease in profits and their corresponding dates
+greatest_increase = max(changes)
+greatest_decrease = min(changes)
+greatest_increase_date = months[changes.index(greatest_increase) + 1]
+greatest_decrease_date = months[changes.index(greatest_decrease) + 1]
+
+# Print the results
+print("Financial Analysis")
+print("------------------------")
+print(f"Total Months: {total_months}")
+print(f"Total: ${net_total}")
+print(f"Average Change: ${average_change:.2f}")
+print(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})")
+print(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})")
 
 # Define the path for the output file
-output_file = r"C:\Users\cryst\Documents\UMN Data Visualization and Analytics Bootcamp\Coursework\Section 2 Data Analytics with Python\Module 3\Challenge Files\Starter_Code\PyPoll\Resources\election_analysis.txt"
+output_file = r"C:\Users\cryst\Documents\UMN Data Visualization and Analytics Bootcamp\Coursework\Section 2 Data Analytics with Python\Module 3\Challenge Files\Starter_Code\PyBank\Resources\budget_analysis.txt"
 
 # Export the results to a text file
 with open(output_file, "w") as textfile:
-    textfile.write(output_string)
+    textfile.write("Financial Analysis\n")
+    textfile.write("---------------------------\n")
+    textfile.write(f"Total Months: {total_months}\n")
+    textfile.write(f"Total: ${net_total}\n")
+    textfile.write(f"Average Change: ${average_change:.2f}\n")
+    textfile.write(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase}\n)")
+    textfile.write(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})\n")
